0347caff23a1f74459eef1c0a1392b69f2dd5e22
LonelyRoze <sam.khalets@gmail.com>
2023-11-13 01:33:04 +0500

    added db connection, button logic

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,15 +32,19 @@
             label1 = new Label();
             label2 = new Label();
             textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            comboBox1 = new ComboBox();
-            comboBox2 = new ComboBox();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            label3 = new Label();
+            label4 = new Label();
+            button2 = new Button();
+            idTextBox = new TextBox();
+            label5 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(316, 342);
+            button1.Location = new Point(317, 327);
             button1.Name = "button1";
             button1.Size = new Size(146, 64);
             button1.TabIndex = 0;
@@ -52,7 +56,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(117, 130);
+            label1.Location = new Point(109, 97);
             label1.Name = "label1";
             label1.Size = new Size(38, 30);
             label1.TabIndex = 1;
@@ -62,7 +66,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(539, 130);
+            label2.Location = new Point(536, 97);
             label2.Name = "label2";
             label2.Size = new Size(25, 30);
             label2.TabIndex = 2;
@@ -70,52 +74,102 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(117, 231);
+            textBox1.Location = new Point(109, 275);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(121, 23);
             textBox1.TabIndex = 3;
             // 
-            // textBox2
+            // listBox1
             // 
-            textBox2.Location = new Point(539, 231);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(121, 23);
-            textBox2.TabIndex = 4;
+            listBox1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 30;
+            listBox1.Items.AddRange(new object[] { "KZT", "RUB", "EUR", "USD" });
+            listBox1.Location = new Point(109, 130);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 94);
+            listBox1.TabIndex = 7;
             // 
-            // comboBox1
+            // listBox2
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(117, 185);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(121, 23);
-            comboBox1.TabIndex = 5;
-            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            listBox2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 30;
+            listBox2.Items.AddRange(new object[] { "KZT", "RUB", "EUR", "USD" });
+            listBox2.Location = new Point(536, 130);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(120, 94);
+            listBox2.TabIndex = 8;
             // 
-            // comboBox2
+            // label3
             // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(539, 185);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(121, 23);
-            comboBox2.TabIndex = 6;
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(109, 242);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 30);
+            label3.TabIndex = 9;
+            label3.Text = "Сумма";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(536, 266);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 30);
+            label4.TabIndex = 10;
+            label4.Text = "label";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(0, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(10, 23);
+            button2.TabIndex = 11;
+            button2.Text = "Админ панель";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // idTextBox
+            // 
+            idTextBox.Location = new Point(317, 73);
+            idTextBox.Name = "idTextBox";
+            idTextBox.Size = new Size(146, 23);
+            idTextBox.TabIndex = 12;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(317, 40);
+            label5.Name = "label5";
+            label5.Size = new Size(142, 30);
+            label5.TabIndex = 13;
+            label5.Text = "ИИН клиента";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(comboBox2);
-            Controls.Add(comboBox1);
-            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(idTextBox);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
             Controls.Add(textBox1);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(button1);
+            Controls.Add(button2);
             Name = "Form1";
             Text = "CurrencyExcanger";
             ResumeLayout(false);
             PerformLayout();
         }
+
+
 
         #endregion
 
@@ -123,8 +177,12 @@
         private Label label1;
         private Label label2;
         private TextBox textBox1;
-        private TextBox textBox2;
-        private ComboBox comboBox1;
-        private ComboBox comboBox2;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Label label3;
+        private Label label4;
+        private Button button2;
+        private TextBox idTextBox;
+        private Label label5;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
 namespace CurrencyExchanger
 {
     public partial class Form1 : Form
@@ -5,18 +8,170 @@ namespace CurrencyExchanger
         public Form1()
         {
             InitializeComponent();
+            listBox1.SelectedIndex = 0;
+            listBox2.SelectedIndex = 1;
+            label4.Text = string.Empty;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            float i;
+            double curr_to_amount = 0.0;
 
-        }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            string[] items = { "Элемент 1", "Элемент 2", "Элемент 3" };
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Поле не может быть пустым!", "Операция невозможна!");
+                return;
+            }
+            else if (!float.TryParse(textBox1.Text, out i))
+            {
+                MessageBox.Show("Вводите только числа!", "Операция невозможна!");
+                return;
+            }
 
-            comboBox1.DataSource = items;
+
+            //операции c тенге\
+            if (listBox1.SelectedItem == "KZT" && listBox2.SelectedItem == "KZT")
+            {
+                MessageBox.Show("Нельзя обменять валюту на неё же!", "Операция невозможна!");
+                return;
+            }
+            if (listBox1.SelectedItem == "KZT" && listBox2.SelectedItem == "RUB")
+            {
+                double conver = i / 5;
+                label4.Text = conver.ToString("F2") + "\t RUB";
+                curr_to_amount = conver;
+            }
+            else if (listBox1.SelectedItem == "KZT" && listBox2.SelectedItem == "EUR")
+            {
+                double conver = i / 500;
+                label4.Text = conver.ToString("F2") + "\t EUR";
+                curr_to_amount = conver;
+            }
+            else if (listBox1.SelectedItem == "KZT" && listBox2.SelectedItem == "USD")
+            {
+                double conver = i / 450;
+                label4.Text = conver.ToString("F2") + "\t USD";
+                curr_to_amount = conver;
+            }
+
+
+            //операции c рублем\
+            if (listBox1.SelectedItem == "RUB" && listBox2.SelectedItem == "RUB")
+            {
+                MessageBox.Show("Нельзя обменять валюту на неё же!", "Операция невозможна!");
+                return;
+            }
+            if (listBox1.SelectedItem == "RUB" && listBox2.SelectedItem == "KZT")
+            {
+                double conver = i * 5;
+                label4.Text = conver.ToString("F2") + "\t KZT";
+                curr_to_amount = conver;
+            }
+            else if (listBox1.SelectedItem == "RUB" && listBox2.SelectedItem == "EUR")
+            {
+                double conver = i / 98;
+                label4.Text = conver.ToString("F2") + "\t EUR";
+                curr_to_amount = conver;
+            }
+            else if (listBox1.SelectedItem == "RUB" && listBox2.SelectedItem == "USD")
+            {
+                double conver = i / 92;
+                label4.Text = conver.ToString("F2") + "\t USD";
+                curr_to_amount = conver;
+            }
+
+
+            //операции c евро\
+            if (listBox1.SelectedItem == "EUR" && listBox2.SelectedItem == "EUR")
+            {
+                MessageBox.Show("Нельзя обменять валюту на неё же!", "Операция невозможна!");
+                return;
+            }
+            if (listBox1.SelectedItem == "EUR" && listBox2.SelectedItem == "KZT")
+            {
+                double conver = i * 500;
+                label4.Text = conver.ToString("F2") + "\t KZT";
+                curr_to_amount = conver;
+            }
+            else if (listBox1.SelectedItem == "EUR" && listBox2.SelectedItem == "RUB")
+            {
+                double conver = i * 98;
+                label4.Text = conver.ToString("F2") + "\t RUB";
+                curr_to_amount = conver;
+            }
+            else if (listBox1.SelectedItem == "EUR" && listBox2.SelectedItem == "USD")
+            {
+                double conver = i * 1.07;
+                label4.Text = conver.ToString("F2") + "\t USD";
+                curr_to_amount = conver;
+            }
+
+
+            //операции c долларом\
+            if (listBox1.SelectedItem == "USD" && listBox2.SelectedItem == "USD")
+            {
+                MessageBox.Show("Нельзя обменять валюту на неё же!", "Операция невозможна!");
+                return;
+            }
+            if (listBox1.SelectedItem == "USD" && listBox2.SelectedItem == "KZT")
+            {
+                double conver = i * 450;
+                label4.Text = conver.ToString("F2") + "\t KZT";
+                curr_to_amount = conver;
+            }
+            else if (listBox1.SelectedItem == "USD" && listBox2.SelectedItem == "RUB")
+            {
+                double conver = i * 92;
+                label4.Text = conver.ToString("F2") + "\t RUB";
+                curr_to_amount = conver;
+            }
+            else if (listBox1.SelectedItem == "USD" && listBox2.SelectedItem == "EUR")
+            {
+                double conver = i / 1.07;
+                label4.Text = conver.ToString("F2") + "\t EUR";
+                curr_to_amount = conver;
+            }
+
+            string clientID = idTextBox.Text;
+            string curr_from = listBox1.SelectedItem.ToString();
+            string curr_to = listBox2.SelectedItem.ToString();
+            float curr_from_amount = i;
+            DateTime currentTime = DateTime.Now;
+            string time = currentTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+
+            dataBase db = new dataBase();
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("INSERT INTO `transactions` (`iin`, `date`, `curr_fr_type`, `curr_fr_amount`, `curr_to_type`, `curr_to_amount`) VALUES (@iin, @datetime, @currfrom, @currfamount, @currto, @currtamount)", db.getConnection());
+
+            command.Parameters.Add("@iin", MySqlDbType.VarChar).Value = clientID;
+            command.Parameters.Add("@datetime", MySqlDbType.DateTime).Value = time;
+            command.Parameters.Add("@currfrom", MySqlDbType.VarChar).Value = curr_from;
+            command.Parameters.Add("@currfamount", MySqlDbType.Float).Value = curr_from_amount;
+            command.Parameters.Add("@currto", MySqlDbType.VarChar).Value = curr_to;
+            command.Parameters.Add("@currtamount", MySqlDbType.Double).Value = curr_to_amount;
+
+            db.openConnection();
+
+            if (command.ExecuteNonQuery() == 1)
+                MessageBox.Show("Транзакция проведена успешно!");
+            else
+                MessageBox.Show("Транзакция не прошла!");
+
+            db.closeConnection();
+
+
+
+
+
         }
     }
+
+
+
 }
